bc6d6e8c3b49a81fdf4397844034d0555ce7b030
SoryKABA <skabaisidk@groupeisi.com>
2023-10-07 00:56:18 +0000

    Le nouveau format de mes deux fichiers d'accès après la mise en place du controller

--- public/admin.php
+++ public/admin.php
@@ -1,74 +1,14 @@
 <?php
-define('ROOT', '../App' . DIRECTORY_SEPARATOR . 'views/');
-define('ROOTADMIN', '../App' . DIRECTORY_SEPARATOR . 'views/Admin/');
+
 require dirname(__DIR__) . '/App/App.php';
+
 $app = App::getInstance();
 $app->load();
-//session_destroy();
-if (!$app->getDbAuth('User')->logged()) {
-    header('Location: index.php?page=login');
-    exit();
-}
 
-$page = $_GET['page'] ?? 'index';
+$page = $_GET['page'] ?? 'admin.posts.index';
 
-
-
-ob_start();
-switch ($page) {
-    case 'index':
-        require ROOTADMIN . 'posts/index.php';
-        break;
-    case 'new':
-        require ROOTADMIN . 'posts/new.php';
-        break;
-    case 'edit':
-        require ROOTADMIN . 'posts/edit.php';
-        break;
-    case 'logout':
-        require ROOTADMIN . 'users/logout.php';
-        break;
-    case 'profil':
-        require ROOTADMIN . 'users/profil.php';
-        break;
-    case 'users':
-        require ROOTADMIN . 'users/users.php';
-        break;
-    case 'add':
-        require ROOTADMIN . 'users/newUser.php';
-        break;
-    case 'useredit':
-        require ROOTADMIN . 'users/edit.php';
-        break;
-    case 'userdelete':
-        require ROOTADMIN . 'users/delete.php';
-        break;
-    case 'delete':
-        require ROOTADMIN . 'posts/delete.php';
-        break;
-    case 'clients':
-        require ROOTADMIN . 'clients/clients.php';
-        break;
-    case 'categoryposts':
-        require ROOTADMIN . 'categories/categoryposts.php';
-        break;
-    case 'category':
-        require ROOTADMIN . 'categories/index.php';
-        break;
-    case 'categoryEdit':
-        require ROOTADMIN . 'categories/edit.php';
-        break;
-    case 'categoryDelete':
-        require ROOTADMIN . 'categories/delete.php';
-        break;
-    case 'categoryNew':
-        require ROOTADMIN . 'categories/new.php';
-        break;
-    
-    default:
-        # code...
-        break;
-}
-
-$content = ob_get_clean();
-require ROOT . 'template/admindefault.php';
+$pages = explode('.', $page);
+$p = $pages[1];
+$action = $pages[2];
+$controller = '\App\Controller\Admin\\' .ucfirst($p) . 'Controller'; 
+(new $controller())->$action();
--- public/index.php
+++ public/index.php
@@ -1,50 +1,14 @@
 <?php
 
-define('ROOT', '../App' . DIRECTORY_SEPARATOR . 'views/');
 require dirname(__DIR__) . '/App/App.php';
 $app = App::getInstance();
 $app->load();
-function url($page)
-{
-    if (isset($page)) {
-        
-        $uri = explode('?', $_SERVER['REQUEST_URI']);
-        $url = str_replace('page=', '\\', end($uri));
-        if(str_contains($url, '&')){
-            $page = str_replace('&id=', '\\', $url);
-            return $page;
-        }
-        return $url;
-    }
-}
 
 
-$page = $_GET['page'] ?? 'index';
-url($page);
+$page = $_GET['page'] ?? 'posts.index';
 
-
-ob_start();
-switch ($page) {
-    case 'index':
-        require ROOT . 'posts/index.php';
-        break;
-    case 'post':
-        require ROOT . 'posts/post.php';
-        break;
-    case 'login':
-        require ROOT . 'users/login.php';
-        break;
-    case 'logout':
-        require ROOT . 'users/logout.php';
-        break;
-    case 'category':
-        require ROOT . 'categorie/index.php';
-        break;
-    
-    default:
-        # code...
-        break;
-}
-
-$content = ob_get_clean();
-require ROOT . 'template/default.php';
+$pages = explode('.', $page);
+$current = $pages[0];
+$action = $pages[1];
+$controller = '\App\Controller\\' . ucfirst($current) . 'Controller';
+(new $controller())->$action();
